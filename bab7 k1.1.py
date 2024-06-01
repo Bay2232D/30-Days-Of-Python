da98b87509b4f92d5312bde58f828fbe5e475c35
Bay2232D <raditbaswara3@gmail.com>
2024-06-01 21:18:45 +0700

    Create bab7 k1.1.py
    kegiatan praktikum bab7

---
+++ bab7 k1.1.py
@@ -0,0 +1,8 @@
+class UserManager:
+    def__init__(self):
+      self.users = []
+    def add_user(self, user)
+      self.users.append(user)
+    def print_user_report(self):
+      for user in self.users:
+        print(f'User: {user.name}, Email : {user.email}')
